be3b84735b635abed569022898b39e26f8ed4aa8
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2019-02-16 20:53:39 -0800

    Add error test cases

--- sliceconv_test.go
+++ sliceconv_test.go
@@ -3,6 +3,7 @@ package sliceconv
 import (
 	"github.com/pkg/errors"
 	"reflect"
+	"strconv"
 	"testing"
 )
 
@@ -15,13 +16,17 @@ func TestAtoi(t *testing.T) {
 	}{
 		{"Nil slice", nil, nil, nil},
 		{"Empty string slice", []string{}, nil, nil},
-		{"Single string", []string{"1"}, []int{1}, nil},
-		{"Multiple string", []string{"1", "2", "3"}, []int{1, 2, 3}, nil},
+		{"Single valid string", []string{"1"}, []int{1}, nil},
+		{"Multiple valid strings", []string{"1", "2", "3"}, []int{1, 2, 3}, nil},
+		{"Single invalid string", []string{"abc"}, nil, &strconv.NumError{Func: "Atoi", Num: "abc", Err: strconv.ErrSyntax}},
+		{"Multiple invalid strings", []string{"abc", "def", "ghi"}, nil, &strconv.NumError{Func: "Atoi", Num: "abc", Err: strconv.ErrSyntax}},
+		{"Mixed valid and invalid strings", []string{"123", "abc"}, nil, &strconv.NumError{Func: "Atoi", Num: "abc", Err: strconv.ErrSyntax}},
+		{"Out of range", []string{"18446744073709552001"}, nil, &strconv.NumError{Func: "Atoi", Num: "18446744073709552001", Err: strconv.ErrRange}},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			ints, err := Atoi(test.strings)
-			if errors.Cause(err) != test.wantErr {
+			if !reflect.DeepEqual(errors.Cause(err), test.wantErr) {
 				t.Errorf("got: <%v>, want: <%v>", errors.Cause(err), test.wantErr)
 			}
 
